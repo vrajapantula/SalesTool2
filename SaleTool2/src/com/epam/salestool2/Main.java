2f17584cd15c252cef21cad2962dc06708893ddf
VinodRajapantula <rvinodqa2009@gmail.com>
2017-09-09 11:55:38 +0530

    First Commit

---
+++ SaleTool2/src/com/epam/salestool2/Main.java
@@ -0,0 +1,14 @@
+package com.epam.salestool2;
+
+public class Main {
+
+	public static void main(String args[]){
+		displayGreeting();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hi1");				
+	}
+
+
+}
